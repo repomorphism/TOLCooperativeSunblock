a6f3615357d36398d3ae5210ef2b4fc53ccc0395
Hsing Liu <liu.hsing@gmail.com>
2020-03-09 04:39:33 -0400

    Fix with baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-// module.exports = {
-//   devServer: {
-//     proxy: "http://localhost:3001/"
-//   }
-// };
+module.exports = {
+  //   devServer: {
+  //     proxy: "http://localhost:3001/"
+  //   }
+  baseUrl: "/TOLCooperativeSunblock/"
+};
