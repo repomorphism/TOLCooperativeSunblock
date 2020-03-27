85836ece8d6dc2b294f0ccef6e4b329dfac8798c
Hsing Liu <liu.hsing@gmail.com>
2020-03-27 12:25:47 -0400

    s4q2

--- src/components/Section4Q2.vue
+++ src/components/Section4Q2.vue
@@ -2,29 +2,71 @@
   <section id="section4q2">
     <h1 v-if="customData.assignment == 'A'">Question 2/3</h1>
     <h1 v-else>Question 2/2</h1>
-    <div v-if="customData.assignment == 'A'">
-      <p>True or false: the higher the number behind the SPF, the stronger the blocking ability would be</p>
-      <input type="radio" name="true" id="spf-q2" />
-      <label for="true">True</label>
-      <input type="radio" name="false" id="spf-q2" />
-      <label for="false">False</label>
-    </div>
-    <div v-else>
-      <p>True or false: PA is generally marked from one plus to triple plus. The more the plus sign, the weaker it will be to block UVA.</p>
-      <input type="radio" name="true" id="pa-q2" />
-      <label for="true">True</label>
-      <input type="radio" name="false" id="pa-q2" />
-      <label for="false">False</label>
-    </div>
-    <button v-on:click="$emit('proceed')">Submit</button>
+    <MultipleChoiceQuestion
+      v-if="customData.assignment == 'A'"
+      v-on:feedback="onFeedback"
+      v-bind:questionData="s4q2A"
+    />
+    <MultipleChoiceQuestion v-else v-on:feedback="onFeedback" v-bind:questionData="s4q2B" />
+    <p v-if="submitted">{{feedback}}</p>
+    <button v-if="!submitted" v-on:click="onSubmit">Submit</button>
+    <button v-if="submitted" v-on:click="$emit('proceed')">Continue</button>
   </section>
 </template>
 
 <script>
+import MultipleChoiceQuestion from "./MultipleChoiceQuestion.vue";
 export default {
   name: "Section4Q2",
-  props: ["customData"]
+  components: { MultipleChoiceQuestion },
+  props: ["customData"],
+  data: function() {
+    return {
+      feedback: "Please select.",
+      submitted: false,
+      s4q2A: {
+        qid: "s4q2-a",
+        questionText:
+          "True or false: the higher the number behind the SPF, the stronger the blocking ability would be",
+        choices: [
+          { cid: "true", text: "True" },
+          { cid: "false", text: "False" }
+        ],
+        correctChoice: "true",
+        correctFeedback: "Answer: Correct!",
+        incorrectFeedback: "Answer: Wrong. It is true."
+      },
+      s4q2B: {
+        qid: "s4q2-b",
+        questionText:
+          "True or false: PA is generally marked from one plus to triple plus. The more the plus sign, the weaker it will be to block UVA.",
+        choices: [
+          { cid: "true", text: "True" },
+          { cid: "false", text: "False" }
+        ],
+        correctChoice: "false",
+        correctFeedback: "Answer: Correct!",
+        incorrectFeedback: "Answer: Wrong. The sentence is incorrect."
+      }
+    };
+  },
+  methods: {
+    onSubmit: function() {
+      this.submitted = true;
+    },
+    onFeedback: function(data) {
+      this.submitted = false;
+      this.feedback = data.feedback;
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-size: 20px;
+  max-width: 640px;
+  width: 640px;
+  text-align: start;
+}
+</style>
